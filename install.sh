0670794cd6652ff320a0777ab3031eafc6b25f28
aoliang <dlmuxishan411@163.com>
2019-05-23 01:34:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 # install brew
 # install zsh
 # install item2
+# install git
 # install go
 # install go version control
+# install go grpc、goreman ...
 # install docker
+# install shadowsocks
+# install goland
+# install sublime
+# install chrome
